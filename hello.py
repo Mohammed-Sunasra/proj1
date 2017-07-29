d8ac67c32b8196e0eb9a5d61c047a1ec3f9545ac
Mohammed-Sunasra <sunasramohd@gmail.com>
2017-07-29 16:21:28 +0530

    Date function

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
-print "Good morning to All"
-print "Hello"
-print "Hey, Good Afternoon"
+#import the module date.py
+print 'Good morning to All'
+
+print 'Good afternoon to all'
+#I call the function from date.py and print the date
