74ca3ed63f12ab2eb5d8ec3aa9b59e7473716066
rohanhg91 <rohanhgupta91@gmail.com>
2017-07-30 10:58:20 +0530

    save changes

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
 #import the module date.py
 #import the module student
+
+
 print 'Good morning to All'
 
-print 'Good night to all'
+msg = 'Good Night to All'
+
+print msg
+
 #Call the function from date.py and print the date
 #Call the function from fibo module and print the series
 #Create student object and call the method
